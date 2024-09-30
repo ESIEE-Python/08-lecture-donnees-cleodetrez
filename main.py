efc86db5f058f18bf8577d75367321f6fa02ddc3
cleodetrez <cleo.detrez@edu.esiee.fr>
2024-09-30 13:10:11 +0000

    exercice du chapitre des fichiers

--- main.py
+++ main.py
@@ -1,52 +1,72 @@
 #### Imports et définition des variables globales
-import random
-
+"""
+Le module random est importé
+"""
 FILENAME = "listes.csv"
-
 #### Fonctions secondaires
-
-def read_data(filename):
-    """retourne le contenu du fichier <filename>
-
-    Args:
+def read_data(filename) :
+    """Retourne le contenu du fichier <filename>
+    Args :
         filename (str): nom du fichier à lire
-
-    Returns:
-        list: le contenu du fichier (1 list par ligne)
+    Returns :
+        list : le contenu du fichier (1 liste par ligne)
     """
     l = []
+    with open(filename, mode='r', encoding='utf8') as f :
+        s = f.readlines()
+        for ligne in s :
+            elements = ligne.strip().split(";")
+            l.append([int(x) for x in elements])
     return l
 
 def get_list_k(data, k):
-    l = []
-    return l
+    """
+    Retourne la kème liste de data
+    """
+    return data[k]
 
 def get_first(l):
-    return None
+    """
+    Retourne le premier élément de la liste
+    """
+    return l[0]
 
 def get_last(l):
-    return None
+    """
+    Retourne le dernier élément de la liste
+    """
+    return l[-1]
 
 def get_max(l):
-    return None
+    """
+    Retourne le max de la liste
+    """
+    return max(l)
 
 def get_min(l):
-    return None
+    """
+    Retourne le min de la liste
+    """
+    return min(l)
 
-def get_sum(l):
-    return None
+def get_sum(liste):
+    """Retourne la somme des éléments dans une liste de nombres sous forme de chaînes."""
+    return sum(liste)
 
 
 #### Fonction principale
 
 
 def main():
-    pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
+    """
+    Test si la fonction data fonctionne
+    """
+    data = read_data(FILENAME)
+    print(len(data))
+    for i, l in enumerate(data):
+        print(i, l)
+    k = 37
+    print(k, get_list_k(data, 37))
 
 
 if __name__ == "__main__":
